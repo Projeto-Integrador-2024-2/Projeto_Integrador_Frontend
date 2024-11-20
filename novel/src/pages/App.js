f9ec6952810dec80a0f395f06a88c55df5e6be9c
Rafael Machado Wanner <rafamwanner@gmail.com>
2024-11-20 19:55:26 -0300

    em progresso pra pagina de crud dos projetos

--- novel/src/pages/App.js
+++ novel/src/pages/App.js
@@ -3,6 +3,7 @@ import HomePage from "./HomePage";
 import Login from "./Login";
 import Header from "./Header";
 import Register from "./Register";
+import Project from "./Project";
 import ProtectedRoute from "../services/ProtectedRoute";
 import React from "react";
 
@@ -18,6 +19,9 @@ function App() {
 
         {/* Página de Registro - acessível para todos */}
         <Route path="/register" element={<Register />} /> {/* Adiciona a rota de registro */}
+
+        {/* Página de Registro - acessível para todos */}
+        <Route path="/project" element={<Project />} /> {/* Adiciona a rota de registro */}
 
         {/* Página protegida */}
         <Route 
