58a5aaed4d87a3c0dd642f0d3fc26a28e81018d6
unknown <gabiismith2004@gmail.com>
2024-11-21 21:28:05 -0300

    register screen dnv

--- novel/src/pages/Register.js
+++ novel/src/pages/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import api from '../api_access';
 
 export default function Register() {
@@ -15,48 +16,147 @@ export default function Register() {
             }
         } catch (error) {
             if (error.response && error.response.status === 400) {
-                // Exibe um alerta informando que o usuário já foi utilizado
                 alert('O nome de usuário já está em uso. Por favor, escolha outro.');
             } else {
-                // Exibe no console outros erros que possam ter ocorrido
                 console.error(error);
             }
         }
     };
-    
+
+    const styles = {
+        container: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            minHeight: '100vh',
+            backgroundImage: `url('/images/2.png'), linear-gradient(to right, transparent 40%, #ff00d6)`,
+            backgroundBlendMode: 'overlay',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            fontFamily: '"Poppins", sans-serif',
+            paddingTop: '20px',
+        },
+        title: {
+            fontSize: '60px',
+            fontWeight: 'bold',
+            color: '#ff00ff',
+            marginBottom: '10px',
+            textAlign: 'center', // Centraliza o texto
+            textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+            fontFamily: '"Chewy", sans-serif',
+            position: 'absolute', // Permite personalizar a posição
+            top: '10%', // Move para o topo da tela
+            left: '50%', // Centraliza horizontalmente
+            transform: 'translate(-50%, -50%)', // Corrige o alinhamento central
+            display: 'flex',
+            alignItems: 'center',
+            gap: '10px', // Espaçamento entre o texto e a imagem
+        },
+        formContainer: {
+            width: '25%',
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'rgba(255, 171, 226)',
+            borderRadius: '20px',
+            boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.2)',
+            padding: '40px',
+            marginRight: '5%',
+            marginLeft: '5%', // Adiciona um pequeno deslocamento à esquerda
+        },
+        inputLabel: {
+            display: 'block',
+            fontWeight: 'bold',
+            marginBottom: '5px',
+            fontFamily: '"Poppins", sans-serif',
+        },
+        inputField: {
+            width: '100%',
+            padding: '10px',
+            borderRadius: '8px',
+            border: '1px solid #ddd',
+            outline: 'none',
+            boxShadow: 'inset 0px 2px 5px rgba(0, 0, 0, 0.1)',
+            fontFamily: '"Poppins", sans-serif',
+        },
+        submitButton: {
+            width: '100%',
+            padding: '12px',
+            borderRadius: '35px',
+            backgroundColor: '#fff', // Cor do fundo do botão em branco
+            color: '#7e005f', // Cor do texto do botão em rosa
+            fontWeight: 'bold',
+            border: 'none',
+            boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2)',
+            cursor: 'pointer',
+            fontSize: '16px',
+            fontFamily: '"Poppins", sans-serif',
+        },
+        signupText: {
+            fontSize: '14px',
+            color: '#555',
+            fontFamily: '"Poppins", sans-serif',
+        },
+        signupLink: {
+            color: '#7e005f',
+            fontWeight: 'bold',
+            fontFamily: '"Poppins", sans-serif',
+        },
+        loginTitle: {
+            fontSize: '30px',
+            fontWeight: 'bold',
+            color: '#7e005f',
+            marginBottom: '20px', // Distância maior entre o título e o formulário
+            fontFamily: '"Signika Negative", sans-serif', // Fonte usada aqui
+            textAlign: 'center',
+        }
+    };
 
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-6">
-                    <h2 className="text-center">Registrar</h2>
-                    <form onSubmit={handleSubmit}>
-                        <div className="form-floating mb-2">
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Digite seu username"
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                                required
-                            />
-                            <label>Username</label>
-                        </div>
-                        <div className="form-floating mb-2">
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Digite sua senha"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                required
-                            />
-                            <label>Password</label>
-                        </div>
-                        <button type="submit" className="btn btn-primary btn-block">
-                            Registrar
-                        </button>
-                    </form>
+        <div style={styles.container}>
+            <div style={styles.formContainer}>
+                <h1 style={styles.title}>
+                    <span style={{ color: 'white' }}>YOUR</span>{' '}
+                    <span style={{ color: '#7e005f' }}>NOVEL</span>{' '}
+                    <img src="/images/coracao.png" alt="coração" style={{ width: '65px', height: 'auto' }} />
+                </h1>
+                <h2 style={styles.loginTitle}>Create Your Account</h2>
+                <form onSubmit={handleSubmit} style={{ width: '100%' }}>
+                    <div style={{ marginBottom: '20px' }}>
+                        <label style={styles.inputLabel}>Username :</label>
+                        <input
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            placeholder="Enter your username"
+                            style={styles.inputField}
+                            required
+                        />
+                    </div>
+                    <div style={{ marginBottom: '20px' }}>
+                        <label style={styles.inputLabel}>Password :</label>
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Enter your password"
+                            style={styles.inputField}
+                            required
+                        />
+                    </div>
+                    <button type="submit" style={styles.submitButton}>
+                        REGISTER
+                    </button>
+                </form>
+                <div style={{ marginTop: '20px', textAlign: 'center' }}>
+                    <p style={styles.signupText}>
+                        Already have an account?{' '}
+                        <Link to="/login" style={styles.signupLink}>
+                            Login now
+                        </Link>
+                    </p>
                 </div>
             </div>
         </div>
